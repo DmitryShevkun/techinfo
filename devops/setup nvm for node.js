e12bdc4a441c879f20dfddfa2d6e0660440d77cd
DmitryShevkun <46721558+DmitryShevkun@users.noreply.github.com>
2019-04-04 15:38:18 +0300

    Create setup nvm for node.js

---
+++ devops/setup nvm for node.js
@@ -0,0 +1,7 @@
+https://www.liquidweb.com/kb/install-nvm-node-version-manager-node-js-ubuntu-16-04-lts/
+- Installing NVM (Node Version Manager) on Ubuntu 16.04 LTS
+apt-get update
+apt-get install build-essential libss1-dev
+curl-o-https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+ - Close and reopen your terminal to start using nvm
+ nvm --version
